77bebc0e253f9a09f9c9753f458ac8cba7c2a982
akima <akima@groovenauts.jp>
2012-05-31 11:05:18 +0900

    [tengine_exampleのrake specが通らない] tengine_jobへの変更がtengine_exampleに反映されていなかったので修正しました。

--- spec/app/event_dsl/uc52_never_commit_event_unless_all_handler_submit_spec.rb
+++ spec/app/event_dsl/uc52_never_commit_event_unless_all_handler_submit_spec.rb
@@ -19,13 +19,15 @@ describe "uc52_never_commit_event_unless_all_handler_submit" do
 
   it "一つsubmitしないハンドラがあるのでackされません" do
     context = @kernel.context
-    @kernel.ack_policies.should == {"event52_alt1"=>:after_all_handler_submit}
+    # @kernel.ack_policies.should == {"event52_alt1"=>:after_all_handler_submit}
+    @kernel.ack_policies["event52_alt1"].should == :after_all_handler_submit
     @kernel.ack?.should == nil
     STDOUT.should_receive(:puts).with("handler52_alt1_1 unacknowledged")
     STDOUT.should_receive(:puts).with("handler52_alt1_2 unacknowledged")
     STDOUT.should_receive(:puts).with("handler52_alt1_3 unacknowledged")
     mock_headers = mock(:headers)
     mock_headers.should_not_receive(:ack)
+    mock_headers.stub(:reject).and_return(mock_headers)
     raw_event = Tengine::Event.new(:event_type_name => "event52_alt1")
     @kernel.before_delegate = lambda do
       @kernel.all_submitted?.should == false
